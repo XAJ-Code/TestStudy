7f2a850d5976935a9b3239d41279a7e522e5cefa
张伟竣 <2938795170@qq.com>
2023-12-14 17:49:52 +0800

    配置browseSync+fs监听文件

--- Ejs/index.js
+++ Ejs/index.js
@@ -1,25 +1,47 @@
 // 引入ejs模块
 import ejs from 'ejs';
-import {Marked, marked} from 'marked';
+import { Marked, marked } from 'marked';
 import fs from 'node:fs';
+import browserSync from 'browser-sync';
+let browse;
+const openBrowser = () => {
+    browse = browserSync.create()
+    browse.init({
+        server: {
+            baseDir: './',
+            index: './index.html'
+        }
+    })
+    return browse;
+}
 // 编译模板字符串
 // const template = ejs.compile('Hello <%=name %>')
-const RademeFile = fs.readFileSync('./markdown.md','utf-8')
-const htmlContent = marked.parse(RademeFile)
-const templete1 = ejs.renderFile('./templete.ejs',{title:'我是用marked转化来的',content:htmlContent} , (err, str) => {
-    if (err) {
-        console.log(err);
-        return;
+const init = (callBack) => {
+    const RademeFile = fs.readFileSync('./markdown.md', 'utf-8')
+    const htmlContent = marked.parse(RademeFile)
+    const templete1 = ejs.renderFile('./templete.ejs', { title: '我是用marked转化来的', content: htmlContent }, (err, str) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        const writeStrem = fs.createWriteStream('./index.html', 'utf-8')
+        writeStrem.write(str)
+        writeStrem.close()
+        writeStrem.on('finish', () => {
+            console.log("写入完成");
+            // openBrowser()
+            callBack && callBack()
+        })
+    })
+}
+init(()=>{
+    openBrowser()
+});
+fs.watchFile('./markdown.md', (curr,prev) => {
+    if(curr.mtime !== prev.mtime){
+        // console.log(curr,prev);
+        init(()=>{
+            browse.reload()
+        });
     }
-    const writeStrem = fs.createWriteStream('./index.html','utf-8')
-    writeStrem.write(str)
-    writeStrem.close()
-    writeStrem.on('finish',()=>{
-        console.log("写入完成");
-    })  
-})
-fs.watchFile('./markdown.md',()=>{
-    console.log("文件被修改了")
-    //重新加载index.html文件
-    
 })
