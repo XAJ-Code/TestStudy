0899ea876178ef5dde5f70d8d53e948ee218b120
张伟竣 <2938795170@qq.com>
2024-01-03 16:26:54 +0800

    修改

--- vue3/vue-project/vite.config.ts
+++ vue3/vue-project/vite.config.ts
@@ -1,5 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import tailwindcss from 'tailwindcss'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
@@ -15,6 +15,13 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss(),
+      ],
     }
   }
 })
