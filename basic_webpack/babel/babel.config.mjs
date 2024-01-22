c740055c23e2e7a11877da51376623642dd85ccb
张伟竣 <2938795170@qq.com>
2024-01-22 11:36:29 +0800

    修改

--- basic_webpack/babel/babel.config.mjs
+++ basic_webpack/babel/babel.config.mjs
@@ -4,7 +4,8 @@ const presets = [
         {
             "targets": {
                 "chrome": "58", // 指定目标浏览器版本号，例如"58"表示Chrome 58版本以上
-                "ie": "11" // 指定目标浏览器版本号，例如"11"表示IE 11版本以上
+                "ie": "11", // 指定目标浏览器版本号，例如"11"表示IE 11版本以上
+                "esmodules": false // 启用ES模块转换
             },
             // 按需加载
             useBuiltIns: "usage",
