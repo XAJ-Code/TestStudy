f6753367f5a9112635df8d8f553405769b9ee277
张伟竣 <2938795170@qq.com>
2024-01-26 14:11:51 +0800

    sse

--- express/app.js
+++ express/app.js
@@ -23,32 +23,12 @@ app.post("/upload", (req, res) => {
   res.send("<h1>恭喜你,成功调用post请求</h1>");
 });
 
-//设置跨域访问
-app.all("*", function (req, res, next) {
-  //设置允许跨域的域名，*代表允许任意域名跨域
-  res.header("Access-Control-Allow-Origin", "*");
-  //允许的header类型
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Requested-With"
-  );
-  //跨域允许的请求方式
-  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-  // 可以带cookies
-  res.header("Access-Control-Allow-Credentials", true);
-  if (req.method == "OPTIONS") {
-    res.sendStatus(200);
-  } else {
-    next();
-  }
-});
-
 //测试sse
 app.get("/sse", (req, res) => {
   res.setHeader("Content-Type", "text/event-stream");
   res.setHeader("Cache-Control", "no-cache");
   res.setHeader("Connection", "keep-alive");
-
+  res.setHeader("Access-Control-Allow-Origin", "*");
   // 发送消息
   setInterval(() => {
     res.write("data: Hello, world!\n\n");
